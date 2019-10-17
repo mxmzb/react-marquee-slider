f4b7f539e4a91ee8d29c7fbbbccbf6369f6066fd
Maxim Zubarev <maxim.zubarev.dev@icloud.com>
2019-10-17 15:43:48 +0200

    ally

--- example/src/components/people.js
+++ example/src/components/people.js
@@ -7,25 +7,25 @@ const People = () => (
   <div>
     <div style={{ height: 200, paddingBottom: 60 }}>
       <Marquee velocity={25} debug>
-        <img src={`${baseUrl}/02.jpg`} className="photo" />
-        <img src={`${baseUrl}/03.jpg`} className="photo" />
-        <img src={`${baseUrl}/04.jpg`} className="photo" />
-        <img src={`${baseUrl}/06.jpg`} className="photo" />
-        <img src={`${baseUrl}/15.jpg`} className="photo" />
-        <img src={`${baseUrl}/07.jpg`} className="photo" />
-        <img src={`${baseUrl}/14.jpg`} className="photo" />
+        <img src={`${baseUrl}/02.jpg`} className="photo" alt="" />
+        <img src={`${baseUrl}/03.jpg`} className="photo" alt="" />
+        <img src={`${baseUrl}/04.jpg`} className="photo" alt="" />
+        <img src={`${baseUrl}/06.jpg`} className="photo" alt="" />
+        <img src={`${baseUrl}/15.jpg`} className="photo" alt="" />
+        <img src={`${baseUrl}/07.jpg`} className="photo" alt="" />
+        <img src={`${baseUrl}/14.jpg`} className="photo" alt="" />
       </Marquee>
     </div>
 
     <div style={{ height: 200 }}>
       <Marquee velocity={25}>
-        <img src={`${baseUrl}/02.jpg`} className="photo offset" />
-        <img src={`${baseUrl}/03.jpg`} className="photo offset" />
-        <img src={`${baseUrl}/04.jpg`} className="photo offset" />
-        <img src={`${baseUrl}/06.jpg`} className="photo offset" />
-        <img src={`${baseUrl}/15.jpg`} className="photo offset" />
-        <img src={`${baseUrl}/07.jpg`} className="photo offset" />
-        <img src={`${baseUrl}/14.jpg`} className="photo offset" />
+        <img src={`${baseUrl}/02.jpg`} className="photo offset" alt="" />
+        <img src={`${baseUrl}/03.jpg`} className="photo offset" alt="" />
+        <img src={`${baseUrl}/04.jpg`} className="photo offset" alt="" />
+        <img src={`${baseUrl}/06.jpg`} className="photo offset" alt="" />
+        <img src={`${baseUrl}/15.jpg`} className="photo offset" alt="" />
+        <img src={`${baseUrl}/07.jpg`} className="photo offset" alt="" />
+        <img src={`${baseUrl}/14.jpg`} className="photo offset" alt="" />
       </Marquee>
     </div>
   </div>
