b794b6654689e740b720c81a7d3014b075011609
Maxim Zubarev <maxim.zubarev.dev@icloud.com>
2019-11-22 01:59:34 +0100

    lodash optimization

--- example/src/components/companies.js
+++ example/src/components/companies.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import _ from "lodash";
+import times from "lodash/times";
 import Marquee, { Motion, randomIntFromInterval } from "react-marquee-slider";
 import { withSize } from "react-sizeme";
 import nanoid from "nanoid";
@@ -94,7 +94,7 @@ const Companies = ({ size }) => {
     <FullWidth>
       <Height height={500}>
         <Marquee key={key} velocity={12} scatterRandomly minScale={0.7} resetAfterTries={200} debug>
-          {_.times(8, Number).map(id => (
+          {times(8, Number).map(id => (
             <Motion
               key={`marquee-example-company-${id}`}
               initDeg={randomIntFromInterval(0, 360)}
